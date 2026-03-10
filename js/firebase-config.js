8ad6485df33a1830615dd24862d4e5741e167d2f
Trendy World <lucas.scribe01@gmail.com>
2026-03-10 10:38:55 +0100

    Set up Firebase configuration and exports

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,23 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js';
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js';
+// import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDIBkKxqnP3sQmDjNL2Dik7NBMhtPBZiKM",
+  authDomain: "mes-raccourcis.firebaseapp.com",
+  projectId: "mes-raccourcis",
+  storageBucket: "mes-raccourcis.firebasestorage.app",
+  messagingSenderId: "954336668667",
+  appId: "1:954336668667:web:391ec66170f5472a87f95e",
+  measurementId: "G-M4NTTWMC1H"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+// Optionnel
+// export const analytics = getAnalytics(app);

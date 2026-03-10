7569acaa4566ea0ccae27e2fa630a7e66eb1c027
Trendy World <lucas.scribe01@gmail.com>
2026-03-10 10:36:41 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,14 @@
+import { signInWithPopup, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js';
+import { auth, provider } from './firebase-config.js';
 
+export async function loginWithGoogle() {
+  return signInWithPopup(auth, provider);
+}
+
+export async function logoutUser() {
+  return signOut(auth);
+}
+
+export function observeAuthState(callback) {
+  return onAuthStateChanged(auth, callback);
+}
